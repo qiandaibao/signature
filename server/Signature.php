4912f01e064dc1c4c2ea880d602b41f3b3c8b613
qiandaibao <xuronghua@new4g.cn>
2015-02-03 18:27:51 +0800

    android initial

---
+++ server/Signature.php
@@ -0,0 +1,15 @@
+<?php
+
+class Signature
+{
+    /**
+     * @param $path
+     * @param $width
+     * @param $height
+     * @return string base64图片
+     */
+    public function redraw($path, $width, $height)
+    {
+        return "";
+    }
+} 
